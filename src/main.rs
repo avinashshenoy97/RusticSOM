fd374467c6300e1e8154f2df27e3eb83e9b04314
Avinash Shenoy <avi123shenoy@hotmail.com>
2018-03-09 12:41:35 +0530

    Added methods of SOM

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,26 @@ extern crate rand;
 extern crate ndarray;
 
 use rand::random as random;
-use ndarray::{Array3, Axis};
+use ndarray::{Array2, Array3, Axis};
 use std::fmt;
+use std::f64::consts::PI as PI;
 
 pub struct SOM {
-    x: usize,           // length of SOM
-    y: usize,           // breadth of SOM
-    z: usize,           // size of inputs
-    map: Array3<f64>,   // the SOM itself
+    x: usize,               // length of SOM
+    y: usize,               // breadth of SOM
+    z: usize,               // size of inputs
+    learning_rate: f32,   // initial learning rate
+    sigma: f64,           // spread of neighbourhood function, default = 1.0
+    map: Array3<f64>,       // the SOM itself
+    decay_function: fn(f64, u64, u64) -> f64,          // the function used to decay learning_rate and sigma
+    neighbourhood_function: fn((usize, usize), (usize, usize), f64) -> Array2<f64>,          // the function used to decay learning_rate and sigma
 }
 
 // Method definitions of the SOM struct
 impl SOM {
 
     // To create a Self-Organizing Map (SOM)
-    fn create(length: usize, breadth: usize, inputs: usize, randomize: bool) -> SOM {
+    fn create(length: usize, breadth: usize, inputs: usize, randomize: bool, learning_rate: Option<f32>, sigma: Option<f64>, decay_function: Option<fn(f64, u64, u64) -> f64>, neighbourhood_function: Option<fn((usize, usize), (usize, usize), f64) -> Array2<f64>>) -> SOM {
         // Map of "length" x "breadth" is created, with depth "inputs" (for input vectors accepted by this SOM)
         // randomize: boolean; whether the SOM must be initialized with random weights or not
 
@@ -32,9 +37,26 @@ impl SOM {
             x: length,
             y: breadth,
             z: inputs,
+            learning_rate: match learning_rate {
+                None => 0.5,
+                Some(value) => value,
+            },
+            sigma: match sigma {
+                None => 1.0,
+                Some(value) => value,
+            },
+            decay_function: match decay_function {
+                None => default_decay_function,
+                Some(foo) => foo,
+            },
+            neighbourhood_function: match neighbourhood_function {
+                None => gaussian,
+                Some(foo) => foo,
+            },
             map: the_map,
         }
     }
+
 }
 
 // To enable SOM objects to be printed with "print" and it's family of formatted string printing functions
@@ -56,8 +78,45 @@ impl fmt::Display for SOM {
     }
 }
 
+fn default_decay_function(val: f64, curr_iter: u64, max_iter: u64) -> f64 {
+    (val as f64) / ((1 + (curr_iter/max_iter)) as f64)
+}
+
+// Default neighbourhood function: Gaussian function; returns a Gaussian centered in pos
+fn gaussian(size: (usize, usize), pos: (usize, usize), sigma: f64) -> Array2<f64> {
+    let mut ret = Array2::<f64>::zeros((size.0, size.1));
+    let div = 2.0 * PI * sigma * sigma;
+
+    let mut x: Vec<f64> = Vec::new();
+    let mut y: Vec<f64> = Vec::new();
+
+    for i in 0..size.0 {
+        x.push(i as f64);
+        if let Some(elem) = x.get_mut(i) {
+            *elem = -((*elem - (pos.0 as f64)).powf(2.0) / div);
+            *elem = (*elem).exp();
+        }
+    }
+
+    for i in 0..size.1 {
+        y.push(i as f64);
+        if let Some(elem) = y.get_mut(i) {
+            *elem = -((*elem - (pos.1 as f64)).powf(2.0) / div);
+            *elem = (*elem).exp();
+        }
+    }
+
+    for i in 0..size.0 {
+        for j in 0..size.1 {
+            ret[[i, j]] = x[i] * y[j];
+        }
+    }
+
+    ret
+}
+
 // Temporary main function for testing, should be removed when converted to proper library!
 fn main() {
-    let map = SOM::create(2, 3, 5, true);
+    let map = SOM::create(2, 3, 5, true, Some(0.1), None, None, None);
     println!("{}", map);
 }
