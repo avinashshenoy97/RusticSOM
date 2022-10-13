5156df095f6446ca61ae9b654b358d47874b628d
beyarkay <boydrkane@gmail.com>
2022-10-13 16:42:46 +0200

    Add documentation and rename  variables

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,12 @@ use std::fmt;
 /// Contains the data about the Self Organising Map
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SomData {
-    /// Length of SOM
+    /// Length of the SOM
     x: usize,
-    /// Breadth of SOM
+    /// Breadth of the SOM
     y: usize,
-    /// size of inputs
+    /// The number of elements in each neuron. Must equal the number of features in each sample of
+    /// the input data
     z: usize,
     /// initial learning rate
     learning_rate: f32,
@@ -100,35 +101,39 @@ impl SOM {
         }
     }
 
-    /// Find and return the position of the winner neuron for a given input sample.
+    /// Find and return the position of the winner neuron for a given input sample. This is called
+    /// the Best Matching Unit (BMU) in the literature
     ///
     /// TODO: (breaking-change) switch `elem` to `ArrayView1`. See todo
     ///       for `Self::winner_dist()`.
     pub fn winner(&mut self, elem: Array1<f64>) -> (usize, usize) {
-        let mut temp: Array1<f64> = Array1::<f64>::zeros(self.data.z);
-        let mut min: f64 = std::f64::MAX;
-        let mut ret: (usize, usize) = (0, 0);
+        let mut delta: Array1<f64> = Array1::<f64>::zeros(self.data.z);
+        let mut min_magnitude = f64::MAX;
+        let mut winning_neuron = (0, 0);
 
         for i in 0..self.data.x {
             for j in 0..self.data.y {
+                // delta = &self.data.map.slice(s![i, j, ..]) - &elem.view();
+                // TODO this assertion just checks that the above broadcasting slice doesn't change
+                // anything. the below assertion can be removed if nothing breaks for a while
                 for k in 0..self.data.z {
-                    temp[k] = self.data.map[[i, j, k]] - elem[[k]];
+                    // assert_eq!(delta[k], self.data.map[[i, j, k]] - elem[[k]]);
+                    delta[k] = self.data.map[[i, j, k]] - elem[[k]];
                 }
 
-                let norm = norm(temp.view());
+                let magnitude = norm(delta.view());
 
-                if norm < min {
-                    min = norm;
-                    ret = (i, j);
+                if magnitude < min_magnitude {
+                    min_magnitude = magnitude;
+                    winning_neuron = (i, j);
                 }
             }
         }
-
-        if let Some(elem) = self.data.activation_map.get_mut(ret) {
-            *(elem) += 1;
+        // Increment the winning neuron's activation
+        if let Some(elem) = self.data.activation_map.get_mut(winning_neuron) {
+            *elem += 1;
         }
-
-        ret
+        winning_neuron
     }
 
     /// Use the provided JSON string to create a SOM. The JSON string should have come from
@@ -152,24 +157,33 @@ impl SOM {
         serde_json::to_string_pretty(&self.data)
     }
 
-    /// Update the weights of the SOM
+    /// Update the weights of the SOM given a sample `elem`, the winning neuron for that sample
+    /// `winner`, and the iteration number `iteration_index`.
     fn update(&mut self, elem: Array1<f64>, winner: (usize, usize), iteration_index: u32) {
+        // Decay the learning rate based on the decay function
         let new_lr = (self.decay_fn)(
             self.data.learning_rate,
             iteration_index,
             self.data.regulate_lrate,
         );
-        let new_sig = (self.decay_fn)(self.data.sigma, iteration_index, self.data.regulate_lrate);
+        // Decay the value of sigma based on the decay function
+        let new_sigma = (self.decay_fn)(self.data.sigma, iteration_index, self.data.regulate_lrate);
 
         let g =
-            (self.neighbourhood_fn)((self.data.x, self.data.y), winner, new_sig as f32) * new_lr;
+            (self.neighbourhood_fn)((self.data.x, self.data.y), winner, new_sigma as f32) * new_lr;
 
+        // Iterate over every neuron
         for i in 0..self.data.x {
             for j in 0..self.data.y {
+                // Iterate over every value in the current neuron
                 for k in 0..self.data.z {
-                    self.data.map[[i, j, k]] += (elem[[k]] - self.data.map[[i, j, k]]) * g[[i, j]];
+                    // Get the difference between the the new neuron value and the old neuron value
+                    let delta = elem[[k]] - self.data.map[[i, j, k]];
+                    // Update the neuron to be the change multiplied by the result of the
+                    // neighbourhood function
+                    self.data.map[[i, j, k]] += g[[i, j]] * delta;
                 }
-
+                // Normalise all of the values of this neuron
                 let norm = norm(self.data.map.index_axis(Axis(0), i).index_axis(Axis(0), j));
                 for k in 0..self.data.z {
                     self.data.map[[i, j, k]] /= norm;
@@ -182,19 +196,23 @@ impl SOM {
     /// iterations.
     pub fn train_random(&mut self, data: Array2<f64>, iterations: u32) {
         let mut random_value: i32;
-        let mut temp1: Array1<f64>;
-        let mut temp2: Array1<f64>;
+        let mut sample: Array1<f64>;
+        let mut sample_copy: Array1<f64>;
         self.update_regulate_lrate(iterations);
         for iteration in 0..iterations {
-            temp1 = Array1::<f64>::zeros(ndarray::ArrayBase::dim(&data).1);
-            temp2 = Array1::<f64>::zeros(ndarray::ArrayBase::dim(&data).1);
+            sample = Array1::<f64>::zeros(ndarray::ArrayBase::dim(&data).1);
+            sample_copy = Array1::<f64>::zeros(ndarray::ArrayBase::dim(&data).1);
+            // Randomly choose an index from `data`
             random_value = rand::thread_rng().gen_range(0, ndarray::ArrayBase::dim(&data).0 as i32);
+            // Iterate over all values in the randomly chosen sample and add them to an array
             for i in 0..ndarray::ArrayBase::dim(&data).1 {
-                temp1[i] = data[[random_value as usize, i]];
-                temp2[i] = data[[random_value as usize, i]];
+                sample[i] = data[[random_value as usize, i]];
+                sample_copy[i] = data[[random_value as usize, i]];
             }
-            let win = self.winner(temp1);
-            self.update(temp2, win, iteration);
+            // Find the winner neuron for the randomly chosen sample
+            let winner = self.winner(sample);
+            // Update the map based on the randomly chosen sample's results
+            self.update(sample_copy, winner, iteration);
             // Iterate over the callbacks and execute them so the user can hook into training
             for cb in self.callbacks.clone() {
                 (cb)(self, iteration);
@@ -235,7 +253,8 @@ impl SOM {
     }
 
     /// Return both the coordinates of the winning neuron for a given sample, as well as the
-    /// distance from that sample to the neuron. Similar to `SOM::winner()`.
+    /// distance from that sample to the neuron. Similar to `SOM::winner()`. The winner is called
+    /// the Best Matching Unit (BMU) in the literature
     ///
     /// TODO: (breaking-change) make `elem` an `ArrayView1` to remove
     ///       at least one heap allocation. Requires same change to
